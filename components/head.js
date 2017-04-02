c7cd9c2d93769fb94ec5c45df9bd5fb0da4f68ef
erfanio <dev@erfan.io>
2017-04-02 12:45:40 +1000

    Fix the links to change route

--- components/head.js
+++ components/head.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Router from 'next/router'
 import { Container, Menu } from 'semantic-ui-react'
 
 export default class extends React.Component {
@@ -10,47 +11,57 @@ export default class extends React.Component {
   }
 
   handleItemClick(e, { name }) {
-    console.log(name)
     this.setState({ activeItem: name })
+
+    let route
+    switch(name) {
+      case 'home':
+        route = ''
+        break
+
+      default:
+        route = name
+    }
+    Router.push(`/${route}`)
   }
 
   render () {
-    console.log('render')
     const { activeItem } = this.state
     const { title } = this.props
-    return (<Container>
-      <Head>
-        <title>UNIHACK - {title}</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"></link>
-      </Head>
+    return (
+      <Container>
+        <Head>
+          <title>UNIHACK - {title}</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"></link>
+        </Head>
 
-      <Menu>
-        <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
-          onClick={this.handleItemClick}
-        >
-          Home
-        </Menu.Item>
+        <Menu>
+          <Menu.Item
+            name='home'
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          >
+            Home
+          </Menu.Item>
 
-        <Menu.Item
-          name='register'
-          active={activeItem === 'register'}
-          onClick={this.handleItemClick}
-        >
-          Register
-        </Menu.Item>
+          <Menu.Item
+            name='register'
+            active={activeItem === 'register'}
+            onClick={this.handleItemClick}
+          >
+            Register
+          </Menu.Item>
 
-        <Menu.Item
-          name='login'
-          active={activeItem === 'login'}
-          onClick={this.handleItemClick}
-        >
-          Login
-        </Menu.Item>
-      </Menu>
-    </Container>
+          <Menu.Item
+            name='login'
+            active={activeItem === 'login'}
+            onClick={this.handleItemClick}
+          >
+            Login
+          </Menu.Item>
+        </Menu>
+      </Container>
     )
   }
 
